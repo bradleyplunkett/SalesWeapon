7caf36407bcb5a793e463ca86f4c9b3d348f55bc
Bradley N. Plunkett <bradleyn.plunkett@Bradleys-MacBook-Pro.local>
2021-12-15 16:54:51 -0700

    wasted hours unsuccessfully manipulating the DOM and installed jQuery instead

--- content.js
+++ content.js
@@ -9,27 +9,48 @@
 // console.log('brad!')
 
 // const employerObj = document.querySelector('div[aria-label="Current company"]');
-//         console.log(employerObj)
+// //         console.log(employerObj)
 
-var images = document.getElementsByTagName('img');
-console.log('these are all the images')
-console.dir(images)
+// var images = document.getElementsByTagName('img');
+// console.log('these are all the images')
+// console.dir(images)
 
-console.log('this is the document body',)
-console.dir(document.body)
+// console.log('this is the document body',)
+// console.dir(document.body)
 
-console.log('these are the children elements',)
-console.dir(document.children)
-
-console.log('these are all the elements with the class name t-14 t-normal',)
-console.log(document.getElementsByClassName('t-14 t-normal'))
+// console.log('these are the children elements',)
+// console.dir(document.children)
 
 
-console.log('these are all the elements with the class name t-14 t-normal t-black--light',)
-console.log(document.getElementsByClassName('t-14 t-normal t-black--light'))
+// console.log('these are all the elements with the class name pvs-list ph5 display-flex flex-row flex-wrap',)
+// console.log(document.getElementsByClassName('pvs-list ph5 display-flex flex-row flex-wrap'))
 
-console.log('below is the name of the user')
-let user = document.getElementsByTagName('title')[0];
-console.log(user);
-console.dir(user);
+// let experienceArray = document.getElementsByClassName('pvs-list ph5 display-flex flex-row flex-wrap')
+// console.log('below is experience array')
+// console.log(experienceArray)
+console.log('brad')
 
+
+// console.log('below is the name of the user')
+// let user = document.getElementsByTagName('title')[0];
+// console.log(user);
+// console.dir(user);
+
+// console.log('these are all the elements with the tag "span',)
+// console.log(document.getElementsByTagName('span'))
+
+// console.log('these are all the span elements with the class visually hidden')
+// console.log(document.querySelectorAll('span.visually-hidden'))
+
+
+
+// // let childNodeArray = document.body.childNodes;
+// // let spanArray = [];
+// // childNodeArray.forEach(function(node){
+// //   console.log('each node looks like this')
+// //   console.dir(node)
+// //   }
+// // )
+
+// console.log('this is the spanArray')
+// console.dir(spanArray)
---
+++ newLogic.js
@@ -0,0 +1,3 @@
+const employer = document.getElementById('ui-theme')
+console.log(employer)
+console.log('can you see this?')
