c5a55a981b8210c93fc082af921af92040f2bcb2
Bradley N. Plunkett <bradleyn.plunkett@Bradleys-MacBook-Pro.local>
2021-12-15 13:48:21 -0700

    made the popup bigger

--- background.js
+++ background.js
@@ -1,8 +1,11 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({hide: true}, function() {
-      console.log("Hide image is on");
-    });
-  });
+// chrome.runtime.onInstalled.addListener(function() {
+//     chrome.storage.sync.set({hide: true}, function() {
+//       console.log("Hide image is on");
+//     });
+//   });
+
+//Don't believe I need the above
+
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
